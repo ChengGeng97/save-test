f64c89cc1ad510ded50e4675bcd0bc4fefb0c9da
Source Academy User <Cheng Geng>
2021-05-05 14:06:37 +0800

    Changes made from Source Academy

--- sdjfhsjkdfhsd.js
+++ sdjfhsjkdfhsd.js
@@ -1,15 +1,2 @@
 # save-test
-Changed line again
-Added another line
-ghjdfiguhjfghj
-sdfghfghj
-hfghjghjkhj
-tgthfgh
-fghfg
-hheadgh
-fgh
-fg
-hheadgh
-lktjuhgkfrghy
-ttghjrkghlkfguhj
-edfgkdfgkjdfg
+Deleted everything else
